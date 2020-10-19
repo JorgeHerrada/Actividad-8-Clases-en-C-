32f2cf41b8fb6f56d4c4f6dbfdea2a2ac17309ba
Jorge Herrada <jorge.herrada.28@gmail.com>
2020-10-19 01:25:45 -0500

    actualizzacion

--- main.cpp
+++ main.cpp
@@ -8,11 +8,6 @@ int main()
     Computadora c1 = Computadora("Linux","INTEL core i7","Ice Lake",8);
     Computadora c2 = Computadora("Windows","AMD A6","Radeon 5",4);
 
-    // cout << "Sobrecarga: "<<endl;
-    // cout << c1;
-    // cout << c2;
-
-
     Computadora c3;
     c3.set_s_o("MacOS");
     c3.setProcesador("INTEL core i9");
@@ -24,15 +19,7 @@ int main()
     c4.setProcesador("INTEL core i5");
     c4.setGrafica("Integrados");
     c4.setRam(8);
-
-    // cout << "Sobrecarga: "<<endl;
-    // cout << c3;
-    // cout << c4;
-
-    Computadora c5;
-    cin >> c5;
-
-
+ 
     Laboratorio lab1;
     // lab1.agregarFinal(c1);
     // lab1.agregarFinal(c2);
@@ -40,12 +27,23 @@ int main()
     // lab1.agregarFinal(c4);
     // lab1.agregarFinal(c5);
 
+    //Captura de computadoras 5 y con sobrecarga de >>
+    Computadora c5;
+    cin >> c5;
+
+    //Agregando objetos a la clase administradora con uso de sobrecarga
     lab1 << c1;
     lab1 << c2;
     lab1 << c3;
     lab1 << c4;
     lab1 << c5;
 
+    //Impresión con sobrecarga de << 
+    cout<<"IMPRESION CON SOBRECARGA DE <<"<<endl<<endl;
+    cout << c1 << c2 << c3 << c4 << c5 <<endl<<endl;
+
+    //IMPRESION CON FUNCION mostrar()
+    cout<<"IMPRESION CON FUNCION mostrar()"<<endl<<endl;
     lab1.mostrar();
 
     return 0;
