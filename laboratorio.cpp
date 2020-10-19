91aa99d4e74bd6b2c9ca7207d5785f58f6c0d08e
Jorge Herrada <jorge.herrada.28@gmail.com>
2020-10-19 00:53:47 -0500

    Uso de formato tabla para desplegar informacion

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -26,14 +26,24 @@ void Laboratorio::agregarFinal(const Computadora &c)
 
 void Laboratorio::mostrar()
 {
+    cout << left;
+    cout << setw(20) << "Sistema Operativo";
+    cout << setw(17) << "Procesador";
+    cout << setw(17) << "Grafica";
+    cout << setw(5) << "RAM"<<endl;
+
     for (size_t i = 0; i < contador; i++)
     {
         Computadora &c = arreglo[i];
-        cout<<"Sistema Operativo: "<< c.get_s_o()<<endl;
-        cout<<"Procesador: "<< c.getProcesador()<<endl;
-        cout<<"Grafica: "<< c.getGrafica()<<endl;
-        cout<<"Ram: "<< c.getRam()<<endl;
-        cout<<endl;
+
+        cout << c;
+
+        // cout<<"Sistema Operativo: "<< c.get_s_o()<<endl;
+        // cout<<"Procesador: "<< c.getProcesador()<<endl;
+        // cout<<"Grafica: "<< c.getGrafica()<<endl;
+        // cout<<"Ram: "<< c.getRam()<<endl;
+        // cout<<endl;
     }
+    cout << endl;
     
 }
